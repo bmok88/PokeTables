2d75510ac3de73f7d347765f1803d53d8f5b82c3
Brendan Mok <bmok88@gmail.com>
2017-08-11 17:40:19 -0700

    Find correct API url to retrieve all 150 Pokemon at once

--- client/src/poketable.js
+++ client/src/poketable.js
@@ -1,3 +1,29 @@
-var body = document.body;
+const pokeAPI = 'http://pokeapi.co/api/v2/pokemon/?limit=150';
+const tableBody = document.getElementById('pokebody');
 
+const retrievePokemon = () => {
+  const request = new XMLHttpRequest();
+  request.open('GET', pokeAPI);
+  request.send(null);
+  request.onreadystatechange = function() {
+    var DONE = 4;
+    var OK = 200;
 
+    if (request.readyState !== 4 || request.status !== 200) {
+      return 'Error:' + request.status;
+    }
+
+    console.log(JSON.parse(request.responseText).results);
+    const pokeData = JSON.parse(request.responseText);
+    buildTable(pokeData);
+  }
+}
+
+const buildTable = (pokeData) => {
+  const newRow = document.createElement('tr');
+  const newCol = document.createElement('td');
+  newRow.appendChild(newCol).innerHTML = pokeData.name;
+  newRow.appendChild(newCol).innerHTML = pokeData.id;
+  tableBody.appendChild(newRow);
+  console.log(tableBody)
+}
